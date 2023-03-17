cc328b070df5763c79d5f21072da00ae4637ef5c
B153Pranali <pranaliwakte@gmail.com>
2023-03-17 12:30:30 +0530

    changes by Pranali

--- add.java
+++ add.java
@@ -1,10 +1,10 @@
-public class A
+public class ABC
 {
-	public static void main(String s[])
+	public static void main(String args[])
 	{
 		int a=10;
 		int b=20;
 		int c= a+b;
-		system.out.println(c);
+		system.out.println("Addition"+c);
 	}
 }
